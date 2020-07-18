d646f127f75f75aaf3d1490378f64a3d7628cb24
Sousa <vitor.menezes@capgemini.com>
2020-07-18 11:53:28 -0300

    Tratando erros 403

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -12,11 +12,17 @@ export class ClienteService {
     }
 
     findByEmail(email: string): Observable<ClienteDTO> {
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+
+        //let token = this.storage.getLocalUser().token;
+        //let authHeader = new HttpHeaders({ 'Authorization': 'Bearer ' + token });
+
+
+        return this.http.get<ClienteDTO>(
+            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
-    getImageFromBuket(id: string): Observable<any> {
+    getImageFromBuket(id : string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, { responseType: 'blob' });
+        return this.http.get(url,{responseType : 'blob'});
     }
 }
