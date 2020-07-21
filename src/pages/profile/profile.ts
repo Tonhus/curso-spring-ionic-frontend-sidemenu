864a9fb6bb98c1e9dcb7f82978cccbe07c953c14
Sousa <vitor.menezes@capgemini.com>
2020-07-21 04:33:24 -0300

    Adicionando a captura de imagem pela camera com o cordova e seleção de arquivo para alteração da imagem de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,9 +1,13 @@
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { ImageBucketService } from './../../services/image-bucket.service';
 import { ClienteService } from './../../services/domain/cliente.service';
 import { ClienteDTO } from './../../models/cliente.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
 
 @IonicPage()
 @Component({
@@ -14,13 +18,23 @@ export class ProfilePage {
 
   email: string;
   cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
+  private _READER: any = new FileReader();
+
+  formGroup: FormGroup;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public imageBucketService: ImageBucketService) {
+    public imageBucketService: ImageBucketService,
+    public formBuilder: FormBuilder,
+    private camera: Camera) {
+    this.formGroup = formBuilder.group({
+      file: [, [Validators.required]]
+    });
   }
 
   ionViewDidLoad() {
@@ -43,7 +57,47 @@ export class ProfilePage {
   }
 
   getImageIfExists() {
-    this.imageBucketService.loadImageUrl(this.cliente,this.imageBucketService.profilePrefix);
+    this.imageBucketService.loadImageUrl(this.cliente, this.imageBucketService.profilePrefix);
+  }
+
+  getCameraPicture() {
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // Do something with the new photo
+      this.picture = `data:image/jpeg;base64,${imageData}`;
+    }, (err) => {
+      // Handle error console.log("Camera issue: " + err);
+    });
+    this.cameraOn = false;
+  }
+
+  readUrl(event) {
+    this.handleImageSelection(event).subscribe(
+      response => {
+        this.picture = response;
+      },
+      error => { }
+    );
+
+  }
+
+  handleImageSelection(event: any): Observable<any> {
+    let file: any = event.target.files[0];
+
+    this._READER.readAsDataURL(file);
+    return Observable.create((observer) => {
+      this._READER.onloadend = () => {
+        observer.next(this._READER.result);
+        observer.complete();
+      }
+    });
   }
 
 }
